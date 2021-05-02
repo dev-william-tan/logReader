be6af7c6107a0738da7e91b26b154feff2a37860
William <dev.william.tan@gmail.com>
2021-05-02 14:52:50 +1200

    revised Top 3 url counting function

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const { table } = require("console");
 let fs = require("fs");
-const { maxHeaderSize } = require("http");
 //regex for IP address
 let r = /[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}/gm;
-let urlregex = /(?<=GET)(.*?)(?=H)/gm; 
+let urlregex = /(?<=GET)(.*?)(?=HTTP)/gm; 
 let file = fs.readFileSync("programming-task-example-data.log").toString('utf-8'); 
 
 let IParray = getIP(file);
 let URLarray = getURL(file);
-let IPcount = countIP(IParray);
 
 //retrieving the IP address from the text file 
 function getIP(address){
@@ -33,30 +30,33 @@ function countIP(){
   }); 
   for (const [key] of Object.entries(count)){
     k.push(key);
-  }
-  console.log("Number of Unique IP Addresses: " + k.length);
+  };
+  console.log("Number of Unique URLs: " + k.length);
 }
 
 function getTop3(){
   let count = [];
-  let k = [];
-  let v = [];
   IParray.forEach (num => {
     count[num] = count[num] ? count[num] + 1 : 1;   
   }); 
-  for (const [key, value] of Object.entries(count)){
-    k.push(key);
-    v.push(value);
-  }
-  
-  let getMax = v.sort((a,b) => b-a).slice(0,3);
-  for (let i = 0; i < getMax.length; i++ ){
-    let element = Object.keys(count).find(key => count[key] === getMax[i]);
-    console.log(`Top 3 most repeated IP Addresses are ${element} with ${getMax[i]} repeated times`);
-  } 
-
+  //Object.entries turns the object into Arrays 
+  let sortCount = Object.entries(count).sort(([,a],[,b]) => b-a).slice(0,3);
+  //getting the top 3 after sorting them from highest to lowest. 
+  let firstURL = sortCount[0];
+  let secondURL = sortCount[1];
+  let thirdURL = sortCount[2];
+  console.log(`Top 3 Most visited URLS: \n First: ${firstURL} times. \n Second: ${secondURL} times. \n Third: ${thirdURL} times.`);
 }
 
-console.log(IPcount);
-console.log(getTop3(IParray));
-console.log(URLarray);
+function countUnique(iterable){
+  let val = iterable; 
+  let first10char =  val[1].substring(0,10);
+  console.log("this is 10 letters" + first10char);
+  return new Set(iterable);
+}
+
+//console.log(countUnique(URLarray));
+
+//console.log(countIP(IParray));
+//console.log(getTop3(IParray));
+//console.log(URLarray);
